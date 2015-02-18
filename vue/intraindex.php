7ee1e15259137ee7963450a21ec0e26f1b1d3f6a
Maxsteed <dorj.maxime@gmail.com>
2015-02-18 17:48:49 +0100

    1

--- vue/intraindex.php
+++ vue/intraindex.php
@@ -1,7 +1,12 @@
 <!DOCTYPE html>
-<html lang="fr">  
-  <head>	
-    </head>  
-  <body>
-  </body>
-  </html>
+<html lang="fr"> 
+<head>
+<meta charset="utf-8">	       
+<link rel="stylesheet" href="styles.css" type="text/css" />	
+<title>Index de l'intranet
+</title>	
+  	
+</head>  
+<body>
+</body>
+</html>
