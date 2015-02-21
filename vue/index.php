bf3918c5091c30eef667c7bca33e30c74f47f9d5
gwen0309 <gwen0309@hotmail.fr>
2015-02-21 13:09:42 +0100

    Page accueil cannes

--- vue/index.php
+++ vue/index.php
@@ -1,7 +1,42 @@
 <!DOCTYPE html>
 <html lang="fr">  
-  <head>	
+    <head>
+        <meta charset="utf-8">
+        <link rel="stylesheet" type="text/css" href="styles.css" media="all">	
     </head>  
-  <body>
-  </body>
-  </html>
+    
+    <body>
+        <div id="connexion">
+            <input id="boutonConnexion" class="btnConnexion" type="button" value="Connexion" onClick="document.location='login.php'" alt="Connexion">
+        </div>
+        
+        <header>
+            <div id="logoCannes">
+                <a href="index.php" > <img src="images/logofestival.png" alt="logoCannes"> </a>
+            </div>
+            
+            <div id="banniere">
+                <img src="images/banner.png" alt="Banner" />
+            </div>
+        </header>
+        
+            <nav> 
+                <ul>
+                    <li> <a href="#">A propos</a></li>
+                    <li> <a href="#">Festival 2014 </a></li>
+                    <li> <a href="#">Le quotidien</a></li>
+                    <li> <a href="#">Archive </a></li>
+                </ul>
+            </nav>
+            
+            <div id="galerie"> 
+            
+            </div>
+            
+         <footer>
+        
+        </footer> 
+              
+    </body>
+    
+</html>
