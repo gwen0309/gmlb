84c790ac55a78dad287420ab941cf2450db24527
gwen0309 <gwen0309@hotmail.fr>
2015-03-06 14:46:34 +0100

    Gwen

--- vue/index.php
+++ vue/index.php
@@ -19,7 +19,7 @@
             </div>
             
             <div id="banniere">
-                <img src="images/banner.png" alt="Banner" />
+                <img src="images/bannerok.png" alt="Banner" />
             </div>
         </header>
         
@@ -47,7 +47,16 @@
             </div>
             
          <footer>
-        
+            <h2> Nous suivre ... </h2>
+            <div id=socialnetwork> 
+                <ul>
+                    <li><a href="http://feeds2.feedburner.com/festival-cannes/rss_fr" target="_blank"><img src="images/rss.png" alt="Flux RSS"/></a></li>
+                    <li><a href="http://feeds2.feedburner.com/festivalcannes/podcast_fr" target="_blank"><img src="images/podcast.png" alt="Podcast"/></a></li>
+                    <li><a href="http://www.festival-cannes.com/fr/apps.html" target="_blank"><img src="images/appli.png" alt="Applications"/></a></li>
+                    <li><a href="https://www.facebook.com/pages/Festival-de-Cannes-Page-Officielle/197710070249937" target="_blank"><img src="images/facebook.png" alt="Facebook"/></a></li>
+                    <li><a href="https://twitter.com/FdC_officiel" target="_blank"><img src="images/twitter.png" alt="Twitter"/></a></li>
+                </ul>
+            </div>
         </footer> 
               
     </body>
